2e36b44064b80632b01c9dc0e45e49e192e96159
Morten Brudvik <morten@brudvik.com>
2018-07-14 21:29:37 +0300

    Initial Commit

---
+++ Healthkit Meditation Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Healthkit Meditation Demo
+//
+//  Created by Morten Brudvik on 14/07/2018.
+//  Copyright © 2018 Morten Brudvik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
